90027b1f4a4c21bc89874ec67b6a984d726e7a13
jaysnowe <jaysnowe.pgh@gmail.com>
2021-01-12 12:41:50 -0500

    Working CountCards() to insert number of cards in the footer innerText

--- HTML.js
+++ HTML.js
@@ -8,3 +8,16 @@ function FlipCard(){
 
     question.replaceWith(clone);
 }
+window.onload = function CountCards(){
+    //pinpoint the answer divs that exist
+    //elementList = parentNode.querySelectorAll(selectors);
+    //count the answer divs 
+
+    const container_length = document.querySelectorAll("div.answer").length;
+    console.log(container_length);
+    const counted_cards = document.getElementById("countCards");
+    console.log(counted_cards)
+    //put the #of answerdivs in the footer inner html
+    counted_cards.innerText = container_length;
+    //figure out how to only count the answer divs that have inner html
+}
