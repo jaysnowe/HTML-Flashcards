41d4925d5b04cb712d5fcc0a4b39ef06b7fdc7ee
jaysnowe <jaysnowe.pgh@gmail.com>
2021-01-13 14:49:40 -0500

    FlipCard() adjusted so one card flips

--- HTML.js
+++ HTML.js
@@ -1,23 +1,38 @@
+
 function FlipCard(){
-    //console.log("It lives!");
+    //locate a the question that is currently visible
+    //locate the answer card to replace the question when the button is pressed
+    //figure out how to replace answer card with question card and vice versa
+    //add function call onClick of the showReverse button
+
+
+    console.log("in FlipCard()");
     const question = document.getElementById('question1');
-    const original = document.getElementById(answer1);
-    const clone = original.cloneNode(true);
+    const answer = document.getElementById('answer1');
+    console.log(question);
 
-    while (question.firstChild) question.firstChild.remove();
+    // const cloneAnswer = answer.cloneNode(true);
+    // const cloneQuest = question.cloneNode(true);
 
-    question.replaceWith(clone);
+    if (question.style.display !== "none") 
+    {
+        console.log("if statement of FlipCard()");
+        question.style.display= "none";
+        answer.style.display = "block";
+    }
+    else if (answer.style.display !== "none"){
+        console.log("else if stmt of FlipCard()");
+        answer.style.display="none";
+        question.style.display= "block";
+    }
 }
 window.onload = function CountCards(){
-    //pinpoint the answer divs that exist
-    //elementList = parentNode.querySelectorAll(selectors);
-    //count the answer divs 
-
+    //get the number of answer divs that currently exist
     const container_length = document.querySelectorAll("div.answer").length;
-    console.log(container_length);
+    //locate where this number should go
     const counted_cards = document.getElementById("countCards");
-    console.log(counted_cards)
-    //put the #of answerdivs in the footer inner html
+    //put the #of answerdivs in the footer inner text (rather than innerHTML)
     counted_cards.innerText = container_length;
-    //figure out how to only count the answer divs that have inner html
+
+    //TODO: figure out how to only count the answer divs that have innertext
 }
